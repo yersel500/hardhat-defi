a652a0eb194dd89c2883ca7218af2cee42437ac6
yersel500 <yersel500@gmail.com>
2022-08-02 08:35:00 -0500

    Implement borrow feature

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -13,6 +13,49 @@ async function main() {
   console.log("Depositing...")
   await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
   console.log("Deposited!")
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(lendingPool, deployer)
+  const daiPrice = await getDaiPrice()
+  const amountDaiToBorrow = availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
+  console.log(`You can borrow ${amountDaiToBorrow} DAI`)
+  const amountDaiToBorrowWei = ethers.utils.parseEther(amountDaiToBorrow.toString())
+  // Borrow time
+  const daiTokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+  await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer)
+  await getBorrowUserData(lendingPool, deployer)
+  await repay(amountDaiToBorrowWei, daiTokenAddress, lendingPool, deployer)
+  await getBorrowUserData(lendingPool, deployer)
+}
+
+async function repay(amount, daiAddress, lendingPool, account) {
+  await approveErc20(daiAddress, lendingPool.address, amount, account)
+  const repayTx = await lendingPool.repay(daiAddress, amount, 1, account)
+  await repayTx.wait(1)
+  console.log("Repaid")
+}
+
+async function borrowDai(daiAddress, lendingPool, amountDaiToBorrow, account) {
+  const borrowTx = await lendingPool.borrow(daiAddress, amountDaiToBorrow, 1, 0, account)
+  await borrowTx.wait(1)
+  console.log("You've borrowed!")
+}
+
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    "0x773616E4d11A78F511299002da57A0a94577F1f4"
+  )
+  const price = (await daiEthPriceFeed.latestRoundData())[1]
+  console.log(`The DAI/ETH price is ${price.toString()}`)
+  return price
+}
+
+async function getBorrowUserData(lendingPool, account) {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await lendingPool.getUserAccountData(account)
+  console.log(`You have ${totalCollateralETH} worth of ETH deposited`)
+  console.log(`You have ${totalDebtETH} worth of ETH borrowed`)
+  console.log(`You can borrow ${availableBorrowsETH} worth of ETH.`)
+  return { availableBorrowsETH, totalDebtETH }
 }
 
 // 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
